41f2390ea07ae54f593c4240ad648b21fa620cff
O Deng <86640014+WellWhale@users.noreply.github.com>
2025-09-07 21:41:49 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -43,12 +43,38 @@ export default createStore({
   },
 });
 
-  // actions: {
-  //   async fetchProductList({ commit }) {
-  //     await axios("/api/productList") // ← 여기서 서버로 "가져온다"
-  //       .then((result) => {
-  //         // console.log(result.data);
-  //         commit("SET_PRODUCTS", result.data); // ← 가져온 걸 state에 "저장해라" (뮤테이션 실행)
-  //       });
-  //   },
-  // },
+// import { createStore } from "vuex";
+// import axios from "axios";
+
+// export default createStore({
+//   // 여기가 실제 데이터 창고
+//   state: () => ({
+//     products: [], // 목록 데이터
+//   }),
+
+//   // 컴포넌트에서 쓰기 좋은 읽기 전용 인터페이스
+//   getters: {
+//     productList(state) {
+//       return state.products; // ← ProductList.vue가 여기로 목록을 "읽는다"
+//     },
+//   },
+
+//   // state를 실제로 변경 하는곳, 동기방식
+//   mutations: {
+//     SET_PRODUCTS(state, list) {
+//       state.products = list; // ← 여기서 목록이 state에 "저장된다"
+//     },
+//   },
+
+//   // 비동기방식, 성공/실패에 따라 mutations 호출
+//   actions: {
+//     async fetchProductList({ commit }) {
+//       await axios("/api/productList") // ← 여기서 서버로 "가져온다"
+//         .then((result) => {
+//           // console.log(result.data);
+//           commit("SET_PRODUCTS", result.data); // ← 가져온 걸 state에 "저장해라" (뮤테이션 실행)
+//         });
+//     },
+//   },
+// });
+
